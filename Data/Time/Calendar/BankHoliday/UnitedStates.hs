1b3f050774328ccf52e9865634c9c7960b159c4d
brady.ouren <brady.ouren@gmail.com>
2015-10-04 19:54:23 -0700

    add more holidays

--- Data/Time/Calendar/BankHoliday/UnitedStates.hs
+++ Data/Time/Calendar/BankHoliday/UnitedStates.hs
@@ -11,37 +11,69 @@
 - Thanksgiving Day - 4th Thursday in November
 - Christmas Day - December 25
 
-> For holidays falling on Saturday, Federal Reserve Banks and Branches will be
+- For holidays falling on Saturday, Federal Reserve Banks and Branches will be
   open the preceding Friday. For holidays falling on Sunday, all Federal
-  Reserve Banks and Branches will be closed the following Monday. Expanded
-  schedule can be found at: Federal Reserve System Holidays
+  Reserve Banks and Branches will be closed the following Monday.
 
 -}
+
 module Data.Time.Calendar.BankHoliday.UnitedStates
   (
     isBankHoliday
   , bankHolidays
+  , isWeekday
+  , isWeekend
   ) where
 
 import Data.Time (Day, fromGregorian, toGregorian)
 import Data.Time.Calendar (addDays, toModifiedJulianDay)
-import Data.Time.Calendar.Easter (gregorianEaster)
 
--- perhaps should newtype a Year data type?
+{- bank holidays for a given year -}
 bankHolidays :: Integer -> [Day]
-bankHolidays yy = standardHolidays
+bankHolidays year = standardHolidays
   where
-    standardHolidays = [ ]-- firstMondayIn may ]
+    [jan, feb, may, jun, sep, dec] = map (fromGregorian year) [1,2,5,6,9,12]
+    standardHolidays = [
+        newYearsDay
+      , mlkDay
+      , inaugurationDay
+      , presidentsDay
+      , memorialDay
+      , christmas
+      ]
+    newYearsDay = case wd jan 1 of
+      3 -> jan 3 -- Saturday
+      4 -> jan 2 -- Sunday
+      _ -> jan 1
+    inaugurationDay = case wd jan 20 of
+      3 -> jan 22  -- saturday
+      4 -> jan 21  -- sunday
+      _ -> jan 20
+    mlkDay = 2 `weeksBefore` firstMondayIn feb
+    presidentsDay = 2 `weeksAfter` firstMondayIn feb
+    memorialDay = weekBefore (firstMondayIn jun)
+    christmas = case wd dec 25 of
+      3 -> dec 27  -- saturday
+      4 -> dec 26  -- sunday
+      _ -> dec 25
 
-firstMondayIn mm = addDays (negate $ wd mm 02) (mm 07)
-wd mm dd = toModifiedJulianDay (mm dd) `mod` 7
+    firstMondayIn mm = addDays (negate $ wd mm 02) (mm 07)
+    wd mm dd = toModifiedJulianDay (mm dd) `mod` 7
+
+weeksBefore n = addDays (n * (-7))
+weekBefore = weeksBefore 1
+
+weeksAfter n = addDays (n * 7)
+weekAfter = weeksAfter 1
 
 isBankHoliday :: Day -> Bool
-isBankHoliday d = False
+isBankHoliday d = d `elem` (bankHolidays year)
   where
-    (yy,mm,dd)   = toGregorian d
-    easterSunday = gregorianEaster yy
+    (year,_,_) = toGregorian d
 
-weeksBefore :: Integer -> Day -> Day
-weeksBefore i = addDays ((-7) * i)
+isWeekend :: Day -> Bool
+isWeekend d = toModifiedJulianDay d `mod` 7 `elem` [3,4]
+
+isWeekday :: Day -> Bool
+isWeekday = not . isWeekend
 
--- test/Data/Time/Calendar/BankHoliday/UnitedStatesSpec.hs
+++ test/Data/Time/Calendar/BankHoliday/UnitedStatesSpec.hs
@@ -2,9 +2,6 @@
 
 module Data.Time.Calendar.BankHoliday.UnitedStatesSpec (spec) where
 
-import Control.Applicative
-import Control.Monad
-import Data.Monoid
 import Data.Time
 import Test.Hspec
 import Test.QuickCheck
@@ -15,7 +12,11 @@ spec :: Spec
 spec = do
   describe "bankHolidays" $ do
     it "is always a weekday" $ property
-      $ \yr -> not $ any (\d -> isWeekday d) (bankHolidays yr)
-        where
-          isWeekday d = toModifiedJulianDay d `mod` 7 `elem` [3,4]
+      $ \yr -> all (\d -> isWeekday d) (bankHolidays yr)
+
+    it "falling on a sunday are delegated to following monday" $ property
+      $ \yr -> 
+
+    it "falling on a saturday are open the preceding friday" $ property
+      $ \yr -> 
 
